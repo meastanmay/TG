8a7d894973d5d8c75bd7040e6868f8ae5fee07b8
meastanmay <tanmay151997@gmail.com>
2024-08-31 05:15:27 -0400

    Updated index.html and app.py files

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
-@app.route('/')
-def index():
-    return render_template('index.html')
+
 @app.route('/calculate', methods=['POST'])
 def calculate():
     data = request.get_json()
