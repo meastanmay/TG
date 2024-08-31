1991a2192c81cece3e1ef7372e3ff59f0bb9ec6b
meastanmay <tanmay151997@gmail.com>
2024-08-31 04:59:10 -0400

    Created Backend and Frontend of simple calculator web app

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    data = request.get_json()
+    num1 = data['num1']
+    num2 = data['num2']
+    operation = data['operation']
+
+    if operation == 'add':
+        result = num1 + num2
+    elif operation == 'subtract':
+        result = num1 - num2
+    elif operation == 'multiply':
+        result = num1 * num2
+    elif operation == 'divide':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            return jsonify({'error': 'Division by zero'}), 400
+    else:
+        return jsonify({'error': 'Invalid operation'}), 400
+
+    return jsonify({'result': result})
+
+if __name__ == '__main__':
+    app.run(debug=True)
