377d981cbe5dddd36cddb28569f5498677cb50a5
meastanmay <tanmay151997@gmail.com>
2024-08-31 05:11:07 -0400

    Second commit

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
-
+@app.route('/')
+def index():
+    return render_template('index.html')
 @app.route('/calculate', methods=['POST'])
 def calculate():
     data = request.get_json()
